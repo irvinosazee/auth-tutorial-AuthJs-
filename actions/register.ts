0993007afed3aad09b6bced26e3409f734b4faac
Irvin Osazee <136273899+irvinosazee@users.noreply.github.com>
2025-08-04 12:22:43 +0100

    Create user (01:57:14 - 02:06:30 )

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,11 @@
 "use server"
-import { RegisterSchema } from "@/schemas"
-import * as z from "zod"
 
+import * as z from "zod"
+import bcrypt from "bcryptjs"
+
+import { db } from "@/lib/db"
+import { RegisterSchema } from "@/schemas"
+import { getUserByEmail } from "@/data/user"
 
 export const register = async (values: z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values)
@@ -10,5 +14,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) =>{
         return { error: "Invalid Fields!"}
     }
 
-    return { success : "Email Sent!"}
+    const { name, email, password } = validatedFields.data
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return {
+            error : "Email already in use!"
+        }
+    }
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password : hashedPassword
+        }
+    })
+
+    // TODO: Send verification token email
+
+    return { success : "User Created !"}
 }
